01cf088eafe7035885ff5716546211ebed1af5ec
Thiago Souza <thiagorasouza@gmail.com>
2022-06-17 02:17:00 -0300

    test: ensure that AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,29 @@
+import { AccountMongoRepository } from './account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('AccountMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await MongoHelper.getCollection('accounts').deleteMany({})
+  })
+
+  test('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    })
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email@email.com')
+    expect(account.password).toBe('valid_password')
+  })
+})
