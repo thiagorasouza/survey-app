fa2b2b62d0ee32911ebc1f4e5be95baf9ea9c694
Thiago Souza <thiagorasouza@gmail.com>
2022-06-17 19:34:42 -0300

    feat: return json content-type by default

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Application } from 'express'
 import { bodyParser } from '../middlewares/body-parser'
+import { contentType } from '../middlewares/content-type'
 import { cors } from '../middlewares/cors'
 
 export default (app: Application): void => {
   app.use(cors)
   app.use(bodyParser)
+  app.use(contentType)
 }
