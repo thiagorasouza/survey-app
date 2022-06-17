e6240f8278f9e83e2e92c30b1bd582a7ba50c3b3
Thiago Souza <thiagorasouza@gmail.com>
2022-06-17 19:29:14 -0300

    feat: ensure that requests are parsed as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Application } from 'express'
+import { bodyParser } from '../middlewares/body-parser'
 import { cors } from '../middlewares/cors'
 
 export default (app: Application): void => {
   app.use(cors)
+  app.use(bodyParser)
 }
