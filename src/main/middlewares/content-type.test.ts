4d588e0933333a3bd7d76a3d8d7132de9122c882
Thiago Souza <thiagorasouza@gmail.com>
2022-06-17 19:37:11 -0300

    test: return other reponse content-type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -10,4 +10,14 @@ describe('Content-Type Middleware', () => {
       .get('/test_content_type_middleware')
       .expect('Content-Type', /json/)
   })
+
+  test('should return xml when forced', async () => {
+    app.get('/test_xml_content_type_middleware', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_xml_content_type_middleware')
+      .expect('Content-Type', /xml/)
+  })
 })
