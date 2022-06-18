f7d736beaf939a50f6bb5093e96be528749f49a6
Thiago Souza <thiagorasouza@gmail.com>
2022-06-18 01:54:35 -0300

    feat: ensure that mongodb reconnects if connection is lost

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,7 +6,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(account)
     const { insertedId } = result
     const insertedAccount = await accountCollection.findOne({ _id: insertedId })
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,18 +1,26 @@
 import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
+  uri: '',
   client: null as MongoClient,
 
   connect: async function (uri): Promise<void> {
+    this.uri = uri
     this.client = new MongoClient(uri)
     await this.client.connect()
   },
 
   disconnect: async function (): Promise<void> {
-    await this.client.close()
+    if (this.client) {
+      await this.client.close()
+      this.client = null
+    }
   },
 
-  getCollection: function (name: string): Collection {
+  getCollection: async function (name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   }
 }
